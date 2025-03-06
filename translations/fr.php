5ee9d7a8ea958c0e7b3dc2d4182c4cee8dcbbacc
DenZaiyy <denzaiyy@outlook.fr>
2025-03-06 15:54:20 +0100

    feat: translations

---
+++ translations/fr.php
@@ -0,0 +1,20 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{mtf_tabs}prestashop>mtf_tabs_b15e092031ef58eec4d9e0896181a92a'] = 'MTF Onglets';
+$_MODULE['<{mtf_tabs}prestashop>mtf_tabs_035fcfacd8d9e925e8d6f293e0e954af'] = 'Création d\'un onglet principal pour tous les modules MTF';
+$_MODULE['<{mtf_tabs}prestashop>adminmtfdashboardcontroller_0c9ceca93348e5488dfd838ca9e3160b'] = 'Tableau de bord des modules MTF';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_0c9ceca93348e5488dfd838ca9e3160b'] = 'Tableau de bord des modules MTF';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_e4382dc90797a2cf2169457730d9c369'] = 'Bienvenue sur le tableau de bord des modules MTF. Il s\'agit du centre de gestion de tous vos modules MTF.';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_93b6dd83fedc9c2d0e8fe1ab38521447'] = 'Modules MTF installés';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_b93bfdeabecb7ed984d30a7fd75f1a0d'] = 'Aucun onglet de module MTF n\'a encore été enregistré.';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_4d3d769b812b6faa6b76e1a8abaece2d'] = 'Actif';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_3cab03c00dbd11bc3569afa0748013f0'] = 'Inactif';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_b80f8f24d3d5bcf5c45291443348aff6'] = 'Onglet:';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_514dc036f88ac88d9b1a22e9f9e39bba'] = 'Aller dans l\'onglet du Module';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_a7bb0a8717b2a709096863077f04a24d'] = 'Comment ajouter votre module';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_23d457a2aa444607d54ce4b3982de6a4'] = 'Pour ajouter votre module dans ce menu :';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_88cea8951d2bfd6b480f0909172921cc'] = 'Dans la méthode install() de votre module, appelez la méthode installTab().';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_d7687d997c5e8d89f0faca0b457729c9'] = 'Veillez à ce que l\'ID parent corresponde à l\'ID de l\'onglet Modules MTF.';
+$_MODULE['<{mtf_tabs}prestashop>dashboard_d39ac0886602d49429f051166e889901'] = 'Vous pouvez utiliser Mtf_Tabs::getParentTabId() pour obtenir l\'ID de l\'onglet parent.';
